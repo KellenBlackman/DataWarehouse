de811f9afba01f7e203c6d009f090b29289784c3
KellenBlackman <kblackman@capstoneheadwaters.com>
2020-09-22 23:06:27 -0600

    Added Docstrings

--- create_tables.py
+++ create_tables.py
@@ -4,22 +4,41 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """Drops all the tables in drop_table_queries for the created postgres connection.
+
+    Parameters:
+        cur(psycopg2 cursor): The cursor to use to drop the tables in drop_tables_queries.
+        conn(psycopg2 connection): The connection to the database that holds the Data Warehouse tables.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """Creates all the tables in create_table_queries for the given postgres connection.
+
+    Parameters:
+        cur(psycopg2 cursor): The cursor to use to create the tables in create_tables_queries.
+        conn(psycopg2 connection): The connection to the database that will hold the Data Warehouse tables.
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """The main function to create_tables.py.
+
+    Reads in the database connection details, creates a database connection and cursor to execute commands.
+    Drops all tables in drop_talbes_queries, then creates all tables in create_table_queries.
+    Closes the connection to the database.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(
+        *config['CLUSTER'].values()))
     cur = conn.cursor()
 
     drop_tables(cur, conn)
--- etl.py
+++ etl.py
@@ -4,18 +4,35 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """Executes the queries in copy_table_queries give cursor and database connection.
+
+    Parameters:
+        cur(psycopg2 cursor): The cursor to execute the queries in copy_table_queries.
+        conn(psycopg2 connection): The connection to the database that holds the staging tables.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """Executes the queries in instert_table_queries to insert data into data warehouse tables.
+
+    Parameters:
+        cur(psycopg2 cursor): The cursor to execute the queries.
+        conn(psycopg2 connection): The connection to the data warehouse.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """The main function for etl.py.
+
+    Reads in database parameters, then creates a connection and cursor to execute queries.
+    Copies S3 buckets into staging tables, then inserts data into data warehouse tables.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
